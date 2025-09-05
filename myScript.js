6daafb43b72aee67c62890e815c2dc919c1e54be
Juan Sanchez <sanchezjuan4159@gmail.com>
2025-09-05 13:56:49 -0700

    Feat: add script for question 3

--- myScript.js
+++ myScript.js
@@ -26,6 +26,26 @@ $(document).ready(function() {
     
     }
   });
+  
+    $('#check-answer3').on('click', function() {
+    const $selectedRadio = $('input[name="q3"]:checked');
+    if ($selectedRadio.length > 0) {
+      if ($selectedRadio.parent('label').hasClass('correct')) {
+        $('#result').text('Correct!');
+          alert("Correct!");
+      } else {
+        $('#result').text('Good Try');
+          alert("Incorrect:/");
+      }
+    
+    }
+  });
+  
+  
+  
+  
+  
+  
 });
   
   
