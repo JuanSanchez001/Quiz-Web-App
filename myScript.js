840a1d226aa38e2f17d28471ee4bc4d2be5111bb
Juan Sanchez <sanchezjuan4159@gmail.com>
2025-09-08 13:40:57 -0700

    Feat: add color to results

--- myScript.js
+++ myScript.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     const $selectedRadio = $('input[name="q1"]:checked');
     if ($selectedRadio.length > 0) {
       if ($selectedRadio.parent('label').hasClass('correct')) {
-        $('#result').text('Correct!');
-		 $('#result').css("margin-left", "45%");
+        $('#result1').text('Correct!');
+		 $('#result1').css({"margin-left": "45%" , "color": "SpringGreen"});
           alert("Correct!");
       } else {
-        $('#result').text('Good Try');
+        $('#result1').text('Good Try');
+		 $('#result1').css({"margin-left": "45%" , "color": "Crimson"});
           alert("Incorrect:/");
       }
     
@@ -18,10 +19,12 @@ $(document).ready(function() {
     const $selectedRadio = $('input[name="q2"]:checked');
     if ($selectedRadio.length > 0) {
       if ($selectedRadio.parent('label').hasClass('correct')) {
-        $('#result').text('Correct!');
+        $('#result2').text('Correct!');
+	  $('#result2').css({"margin-left": "45%" , "color": "SpringGreen"});
           alert("Correct!");
       } else {
-        $('#result').text('Good Try');
+        $('#result2').text('Good Try');
+			 $('#result2').css({"margin-left": "45%" , "color": "Crimson"});
           alert("Incorrect:/");
       }
     
@@ -32,10 +35,12 @@ $(document).ready(function() {
     const $selectedRadio = $('input[name="q3"]:checked');
     if ($selectedRadio.length > 0) {
       if ($selectedRadio.parent('label').hasClass('correct')) {
-        $('#result').text('Correct!');
+        $('#result3').text('Correct!');
+		 $('#result3').css({"margin-left": "45%" , "color": "SpringGreen"});
           alert("Correct!");
       } else {
-        $('#result').text('Good Try');
+        $('#result3').text('Good Try');
+			 $('#result3').css({"margin-left": "45%" , "color": "Crimson"});
           alert("Incorrect:/");
       }
     
@@ -46,10 +51,12 @@ $(document).ready(function() {
     const $selectedRadio = $('input[name="q4"]:checked');
     if ($selectedRadio.length > 0) {
       if ($selectedRadio.parent('label').hasClass('correct')) {
-        $('#result').text('Correct!');
+        $('#result4').text('Correct!');
+		 $('#result4').css({"margin-left": "45%" , "color": "SpringGreen"});
           alert("Correct!");
       } else {
-        $('#result').text('Good Try');
+        $('#result4').text('Good Try');
+			 $('#result4').css({"margin-left": "45%" , "color": "Crimson"});
           alert("Incorrect:/");
       }
     
